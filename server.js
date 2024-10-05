f5b8b7fe4d8955cba7f0b0402acf30d6759b5695
hensol1 <46450678+hensol1@users.noreply.github.com>
2024-10-06 01:51:21 +0300

    Server Mongo

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// MongoDB connection
+const uri = process.env.MONGODB_URI || "mongodb+srv://weknowbetteradmin:dMMZV14rCKTYLJXG@cluster0.sbr1j.mongodb.net/we?retryWrites=true&w=majority&appName=Cluster0";
+
+
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
