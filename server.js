f7e86604c3834538eb1d06677b994beea6706423
hensol1 <46450678+hensol1@users.noreply.github.com>
2024-10-06 02:06:21 +0300

    server

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'https://we-one-bay.vercel.app/'
+}));
 app.use(express.json());
 
 // MongoDB connection
